436872a09c8ad14018701926db3aa3e234dde22d
Maor Atar <maoratar17@gmail.com>
2024-01-29 22:19:50 +0200

    Fixed the db problem. Fixed the order in the AppDbContext file!

--- eTicketsProject/Program.cs
+++ eTicketsProject/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -15,9 +17,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
