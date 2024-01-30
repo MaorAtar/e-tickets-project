10e4cf1766c2d7440c06ac6edaf3415b778ed52f
Maor Atar <maoratar17@gmail.com>
2024-01-30 19:01:45 +0200

    Created Actors service interface

---
+++ eTicketsProject/Data/Services/IActorsService.cs
@@ -0,0 +1,13 @@
+﻿using eTicketsProject.Models;
+
+namespace eTicketsProject.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
