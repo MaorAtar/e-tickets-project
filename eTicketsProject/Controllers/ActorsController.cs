0d3dcc5583873ade961bb77a18476baa0c1274b0
Maor Atar <maoratar17@gmail.com>
2024-02-02 16:01:26 +0200

    Added the Delete Actor functionality.

--- eTicketsProject/Controllers/ActorsController.cs
+++ eTicketsProject/Controllers/ActorsController.cs
@@ -31,7 +31,6 @@ namespace eTicketsProject.Controllers
             {
                 return View(actor);
             }
-
             await _service.AddAsync(actor);
             return RedirectToAction(nameof(Index));
         }
@@ -47,7 +46,6 @@ namespace eTicketsProject.Controllers
         public async Task<IActionResult> Edit(int id)
         {
             var actorDetails = await _service.GetByIdAsync(id);
-
             if (actorDetails == null) return View("NotFound");
             return View(actorDetails);
         }
@@ -59,8 +57,23 @@ namespace eTicketsProject.Controllers
             {
                 return View(actor);
             }
-
             await _service.UpdateAsync(id, actor);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            await _service.DeleteAsync(id);
             return RedirectToAction(nameof(Index));
         }
     }
