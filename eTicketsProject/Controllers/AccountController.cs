984ae26bc0e1f4b890a296ab3f1748d184062ab1
Maor Atar <maoratar17@gmail.com>
2024-02-08 16:36:56 +0200

    Created Login View.

---
+++ eTicketsProject/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using eTicketsProject.Data;
+using eTicketsProject.Data.ViewModels;
+using eTicketsProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTicketsProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+        
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login() => View(new LoginVM());
+    }
+}
--- eTicketsProject/Program.cs
+++ eTicketsProject/Program.cs
@@ -1,6 +1,9 @@
 using eTicketsProject.Data;
 using eTicketsProject.Data.Cart;
 using eTicketsProject.Data.Services;
+using eTicketsProject.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,7 +19,15 @@ builder.Services.AddScoped<IOrdersService, OrdersService>();
 
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddMemoryCache();
 builder.Services.AddSession();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -34,6 +45,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 app.UseSession();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
