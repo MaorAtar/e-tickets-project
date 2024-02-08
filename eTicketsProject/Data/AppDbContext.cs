648f0907ca7394fe51b3d917731c5be84e41fd88
Maor Atar <maoratar17@gmail.com>
2024-02-08 14:23:37 +0200

    Started working on the user identites of the application

--- eTicketsProject/Data/AppDbContext.cs
+++ eTicketsProject/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using eTicketsProject.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace eTicketsProject.Data
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext: IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
